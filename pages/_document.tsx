096ddb62a7c36871c21cc5f1f1a5dd6d3c086c4a
Fabi0o <marfab32@gmail.com>
2023-03-08 19:44:13 +0100

    change footer to not be position fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <style>
+          {`
+                    #__next {
+          display: flex;
+          flex-direction: column;
+          height:100%
+          }
+          `}
+        </style>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
