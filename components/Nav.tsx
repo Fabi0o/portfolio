8ab697fdc7377a56d9d8d0e5a987f4f5c2819359
Fabi0o <marfab32@gmail.com>
2023-03-11 18:31:49 +0100

    add highliting of current page on nav menu

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,7 +1,35 @@
 import styles from "@/styles/Nav.module.scss";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Nav = () => {
+  const { asPath } = useRouter();
+
+  useEffect(() => {
+    toggleCurrentClass(asPath, "add");
+  });
+
+  const toggleCurrentClass = (path: string, action: "add" | "remove") => {
+    let current;
+
+    if (path == "/") {
+      current = document.querySelector("#home");
+    } else if (path == "/about") {
+      current = document.querySelector("#about");
+    } else if (path == "/projects") {
+      current = document.querySelector("#projects");
+    } else if (path == "/contact") {
+      current = document.querySelector("#contact");
+    }
+
+    if (action == "add") {
+      current?.classList.add(`${styles.current}`);
+    } else if (action == "remove") {
+      current?.classList.remove(`${styles.current}`);
+    }
+  };
+
   const displayMenu = (e: React.MouseEvent<HTMLElement>) => {
     const target = e.target as HTMLElement;
     const navMenu = document.querySelector(`.${styles.nav_menu}`);
@@ -16,6 +44,8 @@ const Nav = () => {
     const navMenu = document.querySelector(`.${styles.nav_menu}`)!;
     const icone = document.querySelector("i")!;
 
+    toggleCurrentClass(asPath, "remove");
+
     if (navMenu.className == `${styles.nav_menu} ${styles.active}`) {
       navMenu.classList.toggle(styles.active);
       icone.className = "ri-menu-line";
@@ -29,19 +59,19 @@ const Nav = () => {
 
         <div className={styles.nav_menu}>
           <ul className={styles.list}>
-            <li>
+            <li id="home">
               <Link className={styles.link} href="/" onClick={closeMenu}>
                 _hello
               </Link>
             </li>
 
-            <li>
+            <li id="about">
               <Link className={styles.link} href="/about" onClick={closeMenu}>
                 _about-me
               </Link>
             </li>
 
-            <li>
+            <li id="projects">
               <Link
                 className={styles.link}
                 href="/projects"
@@ -52,7 +82,7 @@ const Nav = () => {
             </li>
           </ul>
 
-          <div className={styles.contact}>
+          <div className={styles.contact} id="contact">
             <Link className={styles.link} href="/contact" onClick={closeMenu}>
               _contact-me
             </Link>
