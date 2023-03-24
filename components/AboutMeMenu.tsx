7db9246e1f600c99f17f048f72dd11d1c37ea5b1
Fabi0o <marfab32@gmail.com>
2023-03-24 20:09:34 +0100

    add basic layout for aboutmemenu using grid

--- components/AboutMeMenu.tsx
+++ components/AboutMeMenu.tsx
@@ -1,5 +1,13 @@
+import styles from "@/styles/AboutMeMenu.module.scss";
+
 const AboutMeMenu = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}></div>
+      <div className={styles.topContent}></div>
+      <div className={styles.bottomContent}></div>
+    </div>
+  );
 };
 
 export default AboutMeMenu;
