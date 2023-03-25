f02223e233f51fb75b3e353728baebe7b9515aa6
Fabi0o <marfab32@gmail.com>
2023-03-25 17:53:54 +0100

    add icons with hover effect to menu on about me page

--- components/AboutMeMenu.tsx
+++ components/AboutMeMenu.tsx
@@ -3,7 +3,11 @@ import styles from "@/styles/AboutMeMenu.module.scss";
 const AboutMeMenu = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.menu}></div>
+      <div className={styles.menu}>
+        <i className="ri-terminal-box-fill"></i>
+        <i className="ri-user-4-fill"></i>
+        <i className="ri-gamepad-fill"></i>{" "}
+      </div>
       <div className={styles.topContent}></div>
       <div className={styles.bottomContent}></div>
     </div>
