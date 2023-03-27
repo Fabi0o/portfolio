488d909af6052c6bb5ca9b58c0d82305c6c1922d
Fabi0o <marfab32@gmail.com>
2023-03-27 12:26:38 +0200

    add functionality for displaying dropdown menu body

--- components/DropdownInfo.tsx
+++ components/DropdownInfo.tsx
@@ -4,9 +4,11 @@ import { InfoProps } from "@/types/Info";
 
 const DropdownInfo: React.FC<InfoProps> = ({ currentInfo }) => {
   const dropdownHeader = useRef<HTMLDivElement>(null);
+  const dropdownBody = useRef<HTMLDivElement>(null);
 
   const displayBody = () => {
     dropdownHeader.current!.classList.toggle(`${styles.active}`);
+    dropdownBody.current!.classList.toggle(`${styles.visible}`);
   };
 
   return (
@@ -15,7 +17,20 @@ const DropdownInfo: React.FC<InfoProps> = ({ currentInfo }) => {
         <i className="ri-play-fill"></i>
         {currentInfo}
       </div>
-      <div className={styles.body}></div>
+      <div className={styles.body} ref={dropdownBody}>
+        <div>
+          <i className="ri-arrow-right-s-line"></i>
+          folder 1
+        </div>
+        <div>
+          <i className="ri-arrow-right-s-line"></i>
+          folder 1
+        </div>
+        <div>
+          <i className="ri-arrow-right-s-line"></i>
+          folder 1
+        </div>
+      </div>
     </div>
   );
 };
