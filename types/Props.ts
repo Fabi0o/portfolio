0be84da70aae1c744c61b6d595344e88ff357a46
Fabi0o <marfab32@gmail.com>
2023-03-29 20:00:52 +0200

    add content type and add it to props type

--- types/Props.ts
+++ types/Props.ts
@@ -1,8 +1,11 @@
+import Content from "./Content";
 import Info from "./Info";
 
 interface Props {
   currentInfo: Info;
   setCurrentInfo?: React.Dispatch<React.SetStateAction<Info>>;
+  currentContent?: Content;
+  setCurrentContent?: React.Dispatch<React.SetStateAction<Content>>;
 }
 
 export default Props;
