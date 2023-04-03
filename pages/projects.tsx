c82cac1f3aa47591006d41293dc20379f49bbc35
Fabi0o <marfab32@gmail.com>
2023-04-03 21:19:34 +0200

    add dropdown components for displaying menu

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,9 +1,13 @@
 import styles from "@/styles/Projects.module.scss";
+import DropdownInfo from "@/components/common/DropdownInfo";
 
 const Projects = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.menu}></div>
+      <div className={styles.menu}>
+        <DropdownInfo currentInfo="projects" />
+      </div>
+
       <div className={styles.projects}></div>
     </div>
   );
