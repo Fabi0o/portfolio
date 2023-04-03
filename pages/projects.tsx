3c6a854b7da2a0222f1974575f80a9b95da3484c
Fabi0o <marfab32@gmail.com>
2023-04-03 20:59:54 +0200

    add basic layout for projects page

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,5 +1,12 @@
+import styles from "@/styles/Projects.module.scss";
+
 const Projects = () => {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.menu}></div>
+      <div className={styles.projects}></div>
+    </div>
+  );
 };
 
 export default Projects;
