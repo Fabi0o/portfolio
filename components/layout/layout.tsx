7ed3f69adac8ba479709785be41600ddf19c222b
Fabi0o <marfab32@gmail.com>
2023-04-14 19:05:22 +0200

    create layout folder in components directory for storing all components connected to layout

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -1,5 +1,5 @@
-import Nav from "./common/Nav";
-import Footer from "./common/Footer";
+import Nav from "./Nav";
+import Footer from "./Footer";
 const Layout = ({ children }: any) => {
   return (
     <>
